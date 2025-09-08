9519474208a338380d7f4e199caaef1e1a564b43
Rahul Pandia <rahulpandia@Rahuls-MacBook-Pro.local>
2025-09-08 15:35:06 +1000

    refactoring of code and project clean up

--- fit5032-week5/src/views/HomeView.vue
+++ fit5032-week5/src/views/HomeView.vue
@@ -103,14 +103,8 @@ const reasonMessage = computed(() => {
           <div class="row mb-3">
             <div class="col-md-6 col-sm-6">
               <label for="username" class="form-label">Username</label>
-              <input
-                type="text"
-                class="form-control"
-                id="username"
-                @blur="() => validateName(true)"
-                @input="() => validateName(false)"
-                v-model="formData.username"
-              />
+              <input type="text" class="form-control" id="username" @blur="() => validateName(true)"
+                @input="() => validateName(false)" v-model="formData.username" />
               <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
 
@@ -127,26 +121,15 @@ const reasonMessage = computed(() => {
           <div class="row mb-3">
             <div class="col-md-6 col-sm-6">
               <label for="password" class="form-label">Password</label>
-              <input
-                type="password"
-                class="form-control"
-                id="password"
-                @blur="() => validatePassword(true)"
-                @input="() => validatePassword(false)"
-                v-model="formData.password"
-              />
+              <input type="password" class="form-control" id="password" @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)" v-model="formData.password" />
               <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
             </div>
 
             <div class="col-md-6 col-sm-6">
               <label for="confirm-password" class="form-label">Confirm password</label>
-              <input
-                type="password"
-                class="form-control"
-                id="confirm-password"
-                v-model="formData.confirmPassword"
-                @blur="() => validateConfirmPassword(true)"
-              />
+              <input type="password" class="form-control" id="confirm-password" v-model="formData.confirmPassword"
+                @blur="() => validateConfirmPassword(true)" />
               <div v-if="errors.confirmPassword" class="text-danger">
                 {{ errors.confirmPassword }}
               </div>
@@ -155,34 +138,19 @@ const reasonMessage = computed(() => {
           <div class="row mb-3">
             <div class="col-md-6 col-sm-6">
               <div class="form-check">
-                <input
-                  type="checkbox"
-                  class="form-check-input"
-                  id="isAustralian"
-                  v-model="formData.isAustralian"
-                />
+                <input type="checkbox" class="form-check-input" id="isAustralian" v-model="formData.isAustralian" />
                 <label class="form-check-label" for="isAustralian">Australian Resident?</label>
               </div>
             </div>
           </div>
           <div class="mb-3">
             <label for="reason" class="form-label">Reason for joining</label>
-            <textarea
-              class="form-control"
-              id="reason"
-              rows="3"
-              v-model="formData.reason"
-            ></textarea>
+            <textarea class="form-control" id="reason" rows="3" v-model="formData.reason"></textarea>
             <div v-if="reasonMessage" class="text-success">{{ reasonMessage }}</div>
           </div>
           <div class="mb-3">
             <label for="suburb" class="form-label">Suburb</label>
-            <input
-              type="text"
-              class="form-control"
-              id="suburb"
-              v-model.trim="formData.suburb"
-            />
+            <input type="text" class="form-control" id="suburb" v-model.trim="formData.suburb" />
           </div>
           <div class="text-center">
             <button type="submit" class="btn btn-primary me-2">Submit</button>
@@ -207,12 +175,7 @@ const reasonMessage = computed(() => {
 
   <div class="row mt-5" v-if="submittedCards.length">
     <div class="d-flex flex-wrap justify-content-start">
-      <div
-        v-for="(card, index) in submittedCards"
-        :key="index"
-        class="card m-2"
-        style="width: 18rem"
-      >
+      <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem">
         <div class="card-header">User Information</div>
         <ul class="list-group list-group-flush">
           <li class="list-group-item">Username: {{ card.username }}</li>
